c4fa43f9b82db4fc0e7e3dff99370dabb7dc6a8e
gamante91 <amantegiacomo@gmail.com>
2022-12-09 19:03:47 +0100

    Add equality operators to compare Enum with enumerated value

--- enum.h
+++ enum.h
@@ -934,6 +934,22 @@ inline bool operator ==(const Enum &a, const Enum &b)                          \
     { return a._to_integral() == b._to_integral(); }                           \
                                                                                \
 BETTER_ENUMS_UNUSED BETTER_ENUMS_CONSTEXPR_                                    \
+inline bool operator ==(const Enum &a, Enum::_enumerated b)                    \
+    { return a._to_integral() == b; }                                          \
+                                                                               \
+BETTER_ENUMS_UNUSED BETTER_ENUMS_CONSTEXPR_                                    \
+inline bool operator ==(Enum::_enumerated a, const Enum &b)                    \
+    { return a == b._to_integral(); }                                          \
+                                                                               \
+BETTER_ENUMS_UNUSED BETTER_ENUMS_CONSTEXPR_                                    \
+inline bool operator =!(const Enum &a, Enum::_enumerated b)                    \
+    { return a._to_integral() =! b; }                                          \
+                                                                               \
+BETTER_ENUMS_UNUSED BETTER_ENUMS_CONSTEXPR_                                    \
+inline bool operator =!(Enum::_enumerated a, const Enum &b)                    \
+    { return a =! b._to_integral(); }                                          \
+                                                                               \
+BETTER_ENUMS_UNUSED BETTER_ENUMS_CONSTEXPR_                                    \
 inline bool operator !=(const Enum &a, const Enum &b)                          \
     { return a._to_integral() != b._to_integral(); }                           \
                                                                                \
